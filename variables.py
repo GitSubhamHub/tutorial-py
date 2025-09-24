359bcc1b5fb7934b65ee136b701b1c598d6d96f0
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2025-09-24 22:20:22 +0530

    Add files via upload

--- variables.py
+++ variables.py
@@ -1,5 +1,6 @@
 #learning variables and datatypes in Python
 
+#create and assigning varables in python
 name = "subham"
 bucket = "water"
 age = 23
@@ -28,3 +29,23 @@ age = float(19)
 print(type(age))
 
 print(age)
+
+#assigning multiple values to multiple variables in a single line
+name, bucket, age, sept = "awasthi", "water1", 22, 21.2025
+
+print(name)
+print(bucket)
+print(age)
+print(sept)
+
+#printing  multiple values using single print function using(,+)
+
+print(name, bucket, age, sept)
+
+print(bucket + name)   #concatenation
+#cannot use concatenation for two different datatypes
+
+#naming syntax
+# dont use numbers in the begining of variable (2abs)
+# dont use spl characters (*abc)
+# Variable are case sensitive (Subham and subham are two different variables)
