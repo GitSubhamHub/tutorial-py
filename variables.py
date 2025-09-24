5a202e0b7ee41b1daa2eae0f67d613b697282bbe
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2025-09-24 20:58:29 +0530

    Add files via upload

---
+++ variables.py
@@ -0,0 +1,30 @@
+#learning variables and datatypes in Python
+
+name = "subham"
+bucket = "water"
+age = 23
+sept = 24.2025
+
+#print output
+print(name)
+print(bucket)
+print(age)
+print(sept)
+
+#Identify datatype and print it 
+print(type(name))
+print(type(bucket))
+print(type(age))
+print(type(sept))
+
+#typecasting
+age = 19
+print(type(age))
+
+age = str(19)
+print(type(age))
+
+age = float(19)
+print(type(age))
+
+print(age)
