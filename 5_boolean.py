bb330607280ac1024ed1447a151d0b8e5fd129d9
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2025-10-05 22:58:22 +0530

    Add files via upload

---
+++ 5_boolean.py
@@ -0,0 +1,35 @@
+#computers only understands (1s and 0s) (yes or no) (true or false)
+
+#in python every expression will return either true or false
+
+
+#if the expression is mathematical
+
+# print(10 > 5)
+# print(10 < 5)
+# print(10 == 5)
+# print(10 == 10)
+
+#if the expression is non mathematical
+#still python || return true or false
+#true if the parameter has a value and false if the parameter is empty
+# x = "ramesh"
+# y = 25
+# z = ""
+
+
+#is x true or false?
+#use function called as bool() this || evaluate if the expression is true or false
+
+# print(bool(x))  #this prints true because x is having some value in it
+# print(bool(y))  #this prints true because y is having some value in it
+# print(bool(z))   #this prints false because z is empty
+
+
+a = 10
+b = 15
+
+if(a > b):
+    print("a is greater than b")
+else: 
+    print("no, a is smaller than b")
