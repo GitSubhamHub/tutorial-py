e3fc199bf107051ed3cfb4b99d30e44782d6a0f1
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2025-10-06 22:28:37 +0530

    Add files via upload

---
+++ 6_operators.py
@@ -0,0 +1,50 @@
+#python has around 8 types of operators
+#basically we use operators to perform certain actions or calculations.
+#===========================
+    #Arithmetic operators (+,-,*,/,%)
+    #Assignment operators (=, +=, -=, *=, /=, %= )
+    #Comparison operators (==, !=, >, <, >=, <= )
+    #Logical Operators (and, or, not)
+    #Identity operators (is, is not)
+    #Membership Operators (in, not in)
+    #Bitwise operators
+
+
+#arithmetic operators
+print(10+35)
+print(200-45)
+print(30*20)
+
+
+#assignmet operators
+x = 10
+y = 15
+z = x
+print(z)
+
+x = 10
+# x = x + 10
+x += 10
+print(x)
+
+# x = x - 5
+x -= 5
+print(x)
+
+# x = x*15
+x *= 15
+print(x)
+
+
+#comparison operators 
+print(10==20)
+print(10 > 20)
+print(10 < 20)
+print(10 <= 20)
+print(10 >= 20)
+
+
+#logical operators
+print(10<20 and 10<15)
+print(10>20 and 10<30)
+print(10>20 or 10<30)
