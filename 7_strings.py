7130610037f3ecd0389b9a0d21132e561488d6f9
Subham <132269477+GitSubhamHub@users.noreply.github.com>
2025-10-25 22:46:33 +0530

    Add files via upload

---
+++ 7_strings.py
@@ -0,0 +1,39 @@
+#assigning 
+name = "ramesh"
+x = "this is a random string"
+
+#multiline
+x = """this is the first sentence and 
+this is the second sentence"""
+print(x)
+
+#indexing and negative indexing
+name = "ramesh"
+
+#  r    a    m    e    s    h   
+#  0    1    2    3    4    5   #indexing
+# -6   -5   -4   -3   -2   -1   #negative indexing
+
+print(name[2])       #this prints 2nd character of the string
+print(name[-5])      #this prints -5th character of the string
+print(name[2:])      #this prints from m to end of string
+print(name[:2])      #this prints everything before m
+
+#slicing
+#just print ame
+print(name[1:4])  #included:excluded this is the syntax
+
+#modification (upper, lower, strip)
+name = "ramesh"
+print(name.upper())
+
+name = name.upper()
+print(name)
+
+lowername = name.lower()
+print(lowername)
+
+fname = "ramesh         "
+fname = fname.strip()  #this functin will simply remove all spaces before and after string 
+lname = "kumar"
+print(fname + " " + lname)
